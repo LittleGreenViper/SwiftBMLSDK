fad7751e03e7e13a40a5bb0d848560a7cc2c29c6
Chris Marshall <chris@riftvalleysoftware.com>
2024-02-06 13:54:21 -0500

    Added a model, based on the large dump

--- research/MeetingML.playground/Contents.swift
+++ research/MeetingML.playground/Contents.swift
@@ -3,9 +3,9 @@ import CreateML
 
 @available(macOS 13.0, *)
 
-func loadJSON() -> (meta: MeetingJSONParser.PageMeta, meetings: [MeetingJSONParser.Meeting])? {
+func loadJSON(file inFileSize: String) -> (meta: MeetingJSONParser.PageMeta, meetings: [MeetingJSONParser.Meeting])? {
     print("Getting File")
-    guard let jsonFileURL = Bundle.main.url(forResource: "large-dump", withExtension: "json") else { return nil }
+    guard let jsonFileURL = Bundle.main.url(forResource: "\(inFileSize)-dump", withExtension: "json") else { return nil }
     print("Extracting Data")
     guard let jsonData = try? Data(contentsOf: jsonFileURL) else { return nil }
     print("Parsing Data")
@@ -14,10 +14,21 @@ func loadJSON() -> (meta: MeetingJSONParser.PageMeta, meetings: [MeetingJSONPars
     return (meta: parser.meta, meetings: parser.meetings)
 }
 
-if let results = loadJSON() {
-    print("Training Day!")
-    if let classifier = try? MLTextClassifier(trainingData: results.meetings.taggedStringData) {
-        print(classifier)
+func createModelFile(file inFileSize: String = "small") {
+    if let results = loadJSON(file: inFileSize) {
+        print("Training Day!")
+        if let classifier = try? MLTextClassifier(trainingData: results.meetings.taggedStringData),
+           let modelURL = URL(string: "~/Desktop/NAMeetings.mlmodel") {
+            let metadata = MLModelMetadata(author: "BMLSDK", shortDescription: "NA Meetings", license: "", version: "1.0")
+            print("Get to Work! (\(modelURL))")
+            do {
+                try classifier.write(toFile: modelURL.absoluteString, metadata: metadata)
+            } catch {
+                print("ERROR: \(error)")
+            }
+        }
     }
 }
 
+createModelFile(file: "large")
+exit
