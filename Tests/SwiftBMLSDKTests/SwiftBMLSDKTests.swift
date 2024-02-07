b86eb4facf20c296cbceab565a8bc0fb7e3537a2
Chris Marshall <chris@riftvalleysoftware.com>
2024-02-07 16:09:37 -0500

    Whoops Typo

--- Tests/SwiftBMLSDKTests/SwiftBMLSDKTests.swift
+++ Tests/SwiftBMLSDKTests/SwiftBMLSDKTests.swift
@@ -6,9 +6,9 @@
 //
 
 import XCTest
-@testable import SwiftMLSDK
+@testable import SwiftBMLSDK
 
-final class SwiftMLSDKTests: XCTestCase {
+final class SwiftBMLSDKTests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
