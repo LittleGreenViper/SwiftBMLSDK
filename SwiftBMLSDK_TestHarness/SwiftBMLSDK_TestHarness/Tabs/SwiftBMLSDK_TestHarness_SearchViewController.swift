68bdef1a7cefe0901ce47380cbb23f30214055a1
Chris Marshall <chris@riftvalleysoftware.com>
2024-02-27 16:38:56 -0500

    OK. The basic text items are in place, and working. The open map button is there, but doesn't do anything yet.

--- SwiftBMLSDK_TestHarness/SwiftBMLSDK_TestHarness/Tabs/SwiftBMLSDK_TestHarness_SearchViewController.swift
+++ SwiftBMLSDK_TestHarness/SwiftBMLSDK_TestHarness/Tabs/SwiftBMLSDK_TestHarness_SearchViewController.swift
@@ -47,12 +47,96 @@ class SwiftBMLSDK_TestHarness_SearchViewController: SwiftBMLSDK_TestHarness_TabB
     /**
      */
     @IBOutlet weak var locationToggleSwitch: UISwitch?
+    
+    /* ################################################################## */
+    /**
+     */
+    @IBOutlet weak var locationItemsStackView: UIStackView?
+    
+    /* ################################################################## */
+    /**
+     */
+    @IBOutlet weak var openMapButton: UIButton?
+    
+    /* ################################################################## */
+    /**
+     */
+    @IBOutlet weak var latitudeLabel: UILabel?
+    
+    /* ################################################################## */
+    /**
+     */
+    @IBOutlet weak var latitudeTextField: UITextField?
+    
+    /* ################################################################## */
+    /**
+     */
+    @IBOutlet weak var longitudeLabel: UILabel?
+    
+    /* ################################################################## */
+    /**
+     */
+    @IBOutlet weak var longitudeTextField: UITextField?
+    
+    /* ################################################################## */
+    /**
+     */
+    @IBOutlet weak var radiusLabel: UILabel?
+    
+    /* ################################################################## */
+    /**
+     */
+    @IBOutlet weak var radiusTextField: UITextField?
+}
+
+/* ###################################################################################################################################### */
+// MARK: Static Functions
+/* ###################################################################################################################################### */
+extension SwiftBMLSDK_TestHarness_SearchViewController {
+    /* ################################################################## */
+    /**
+     */
+    static func setFloatingPointTextField(_ inTextField: UITextField?, to inDouble: Double) {
+        inTextField?.text = String(format: "%g", inDouble)
+    }
 }
 
 /* ###################################################################################################################################### */
 // MARK: Callbacks
 /* ###################################################################################################################################### */
 extension SwiftBMLSDK_TestHarness_SearchViewController {
+    /* ################################################################## */
+    /**
+     Called when the open map button is hit
+     
+     - parameter: The button (ignored).
+     */
+    @IBAction func openMapButtonHit(_: Any) {
+    }
+    
+    /* ################################################################## */
+    /**
+     */
+    @IBAction func textFieldTextChanged(_ inTextField: UITextField) {
+        guard let latText = latitudeTextField?.text,
+              let latitude = Double(latText),
+              let lngText = longitudeTextField?.text,
+              let longitude = Double(lngText),
+              let radText = radiusTextField?.text
+        else { return }
+        
+        let center = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+        
+        guard CLLocationCoordinate2DIsValid(center) else { return }
+        
+        let radius = Double(radText) ?? 0
+        
+        guard 0 < radius else { return }
+        
+        SwiftBMLSDK_TestHarness_Prefs().locationCenter = center
+        SwiftBMLSDK_TestHarness_Prefs().locationRadius = radius
+    }
+
     /* ################################################################## */
     /**
      Called when either the label or toggle for the location search row is selected.
@@ -65,10 +149,24 @@ extension SwiftBMLSDK_TestHarness_SearchViewController {
             locationToggleSwitch?.sendActions(for: .valueChanged)
         } else if let toggle = inControl as? UISwitch {
             if toggle.isOn {
-                SwiftBMLSDK_TestHarness_Prefs().locationRadius = Self._defaultRadiusInMeters
-                SwiftBMLSDK_TestHarness_Prefs().locationCenter = SwiftBMLSDK_TestHarness_Prefs().currentUserLocation ?? Self._defaultLocationCenter
+                var center = SwiftBMLSDK_TestHarness_Prefs().locationCenter
+                let radius = 0 < SwiftBMLSDK_TestHarness_Prefs().locationRadius ? SwiftBMLSDK_TestHarness_Prefs().locationRadius : Self._defaultRadiusInMeters
+                
+                if !CLLocationCoordinate2DIsValid(center) {
+                    center = Self._defaultLocationCenter
+                }
+                
+                Self.setFloatingPointTextField(latitudeTextField, to: center.latitude)
+                Self.setFloatingPointTextField(longitudeTextField, to: center.longitude)
+                Self.setFloatingPointTextField(radiusTextField, to: radius)
+                
+                SwiftBMLSDK_TestHarness_Prefs().locationRadius = radius
+                SwiftBMLSDK_TestHarness_Prefs().locationCenter = center
+                
+                locationItemsStackView?.isHidden = false
             } else {
                 SwiftBMLSDK_TestHarness_Prefs().locationRadius = 0
+                locationItemsStackView?.isHidden = true
             }
         }
     }
@@ -84,8 +182,44 @@ extension SwiftBMLSDK_TestHarness_SearchViewController {
      */
     override func viewDidLoad() {
         super.viewDidLoad()
-        locationToggleLabelButton?.setTitle("SLUG-LOCATION-SEARCH-TOGGLE".localizedVariant, for: .normal)
+        locationToggleLabelButton?.setTitle(locationToggleLabelButton?.title(for: .normal)?.localizedVariant, for: .normal)
+        openMapButton?.setTitle(openMapButton?.title(for: .normal)?.localizedVariant, for: .normal)
         locationToggleSwitch?.isOn = SwiftBMLSDK_TestHarness_Prefs().isLocationBasedSearch
+        locationItemsStackView?.isHidden = !SwiftBMLSDK_TestHarness_Prefs().isLocationBasedSearch
+        latitudeLabel?.text = latitudeLabel?.text?.localizedVariant
+        latitudeTextField?.placeholder = latitudeTextField?.placeholder?.localizedVariant
+        longitudeLabel?.text = longitudeLabel?.text?.localizedVariant
+        longitudeTextField?.placeholder = longitudeTextField?.placeholder?.localizedVariant
+        radiusLabel?.text = radiusLabel?.text?.localizedVariant
+        radiusTextField?.placeholder = radiusTextField?.placeholder?.localizedVariant
+        
+        let center = SwiftBMLSDK_TestHarness_Prefs().currentUserLocation ?? Self._defaultLocationCenter
+        let radius = 0 < SwiftBMLSDK_TestHarness_Prefs().locationRadius ? SwiftBMLSDK_TestHarness_Prefs().locationRadius : Self._defaultRadiusInMeters
+        
+        Self.setFloatingPointTextField(latitudeTextField, to: center.latitude)
+        Self.setFloatingPointTextField(longitudeTextField, to: center.longitude)
+        Self.setFloatingPointTextField(radiusTextField, to: radius)
+    }
+}
+
+/* ###################################################################################################################################### */
+// MARK: UITextFieldDelegate Conformance
+/* ###################################################################################################################################### */
+extension SwiftBMLSDK_TestHarness_SearchViewController: UITextFieldDelegate {
+    /* ################################################################## */
+    /**
+     */
+    func textField(_ inTextField: UITextField, shouldChangeCharactersIn inRange: NSRange, replacementString inString: String) -> Bool {
+        guard let testString = (inTextField.text as NSString?)?.replacingCharacters(in: inRange, with: inString),
+              let decimalSeparator = Locale.current.decimalSeparator
+        else { return false }
+
+        var digits = CharacterSet.decimalDigits
+        digits.insert(charactersIn: decimalSeparator)
+        digits.insert(charactersIn: "-")
+        let stringSet = CharacterSet(charactersIn: testString)
+
+        return digits.isSuperset(of: stringSet)
     }
 }
 
