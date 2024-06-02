14746f8f8c5cb1492cd75e73203f1f1d2677189d
Chris Marshall <chris@riftvalleysoftware.com>
2024-06-02 12:30:36 -0400

    Beginning the process of implementing the calendar input.

--- SwiftBMLSDK_TestHarness/SwiftBMLSDK_TestHarness/Tabs/Virtual/SwiftBMLSDK_TestHarness_VirtualCustom1ViewController.swift
+++ SwiftBMLSDK_TestHarness/SwiftBMLSDK_TestHarness/Tabs/Virtual/SwiftBMLSDK_TestHarness_VirtualCustom1ViewController.swift
@@ -28,11 +28,90 @@ import RVS_CalendarInput
 /**
  */
 class SwiftBMLSDK_TestHarness_VirtualCustom1ViewController: SwiftBMLSDK_TestHarness_BaseViewController {
+    /* ################################################################################################################################## */
+    // MARK: Date Item Class (One element of the `data` array)
+    /* ################################################################################################################################## */
+    /**
+     This is one element of the data that is provided to, and read from, the view.
+     */
+    private struct _DateItem: RVS_CalendarInputDateItemProtocol {
+        // MARK: Required Stored Properties
+        /* ############################################################## */
+        /**
+         The year, as an integer. REQUIRED
+         */
+        public var year: Int
+        
+        /* ############################################################## */
+        /**
+         The month, as an integer (1 -> 12). REQUIRED
+         */
+        public var month: Int
+        
+        /* ############################################################## */
+        /**
+         The day of the month (1 -> [28|29|30|31]), as an integer. REQUIRED
+         */
+        public var day: Int
+        
+        // MARK: Optional Stored Properties
+        /* ############################################################## */
+        /**
+         True, if the item is enabled for selection. Default is false. OPTIONAL
+         */
+        public var isEnabled: Bool
+        
+        /* ############################################################## */
+        /**
+         True, if the item is currently selected. Default is false. OPTIONAL
+         */
+        public var isSelected: Bool
+
+        /* ############################################################## */
+        /**
+         Reference context. This is how we attach arbitrary data to the item. OPTIONAL
+         */
+        public var refCon: Any?
+        
+        // MARK: Default Initializer
+        /* ############################################################## */
+        /**
+         Default Initializer. The calendar used, will be the current one.
+         
+         - parameter day: The day of the month (1 -> [28|29|30|31]), as an integer. REQUIRED
+         - parameter month: The month, as an integer (1 -> 12). REQUIRED
+         - parameter year: The year, as an integer. REQUIRED
+         - parameter isEnabled: True, if the item is enabled for selection. Default is false. OPTIONAL
+         - parameter isSelected: True, if the item is currently selected. Default is false. OPTIONAL
+         - parameter refCon: Reference context. This is how we attach arbitrary data to the item. OPTIONAL
+         */
+        public init(day inDay: Int,
+                    month inMonth: Int,
+                    year inYear: Int,
+                    isEnabled inIsEnabled: Bool = false,
+                    isSelected inIsSelected: Bool = false,
+                    refCon inRefCon: Any? = nil
+        ) {
+            day = inDay
+            month = inMonth
+            year = inYear
+            isEnabled = inIsEnabled
+            isSelected = inIsSelected
+            refCon = inRefCon
+        }
+    }
+
     /* ################################################################## */
     /**
      The segue ID to the list display.
      */
     private static let _showListSegueID = "show-list"
+    
+    /* ################################################################## */
+    /**
+     This is the actual widget, in our layout.
+     */
+    @IBOutlet weak var calendarView: RVS_CalendarInput?
 }
 
 /* ###################################################################################################################################### */
@@ -46,5 +125,71 @@ extension SwiftBMLSDK_TestHarness_VirtualCustom1ViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationItem.title = "SLUG-CUSTOM-1-BUTTON".localizedVariant
+        calendarView?.delegate = self
+        setUpWidgetFromDates()
+    }
+    
+    /* ################################################################## */
+    /**
+     This creates a new array of date items, and sets them to the widget.
+     */
+    func setUpWidgetFromDates() {
+        let startDate = Date.now
+        let endDate = startDate.addingTimeInterval(86400 * 90)
+        var seedData = [_DateItem]()
+
+        // Determine a start day, and an end day. Remember that we work in "whole month" increments.
+        if let today = Calendar.current.date(from: Calendar.current.dateComponents([.year, .month, .day], from: Date())) {
+            // What we do here, is strip out the days. We are only interested in the month and year of each end.
+            let startComponents = Calendar.current.dateComponents([.year, .month], from: startDate)
+            let endComponents = Calendar.current.dateComponents([.year, .month], from: endDate)
+            
+            guard let startYear = startComponents.year,
+                  let endYear = endComponents.year,
+                  var startMonth = startComponents.month,
+                  let endMonth = endComponents.month
+            else { return }
+            
+            // Now, a fairly simple nested loop is used to poulate our data.
+            for year in startYear...endYear {
+                for month in startMonth...12 where year < endYear || month <= endMonth {
+                    if let calcDate = Calendar.current.date(from: DateComponents(year: year, month: month)),
+                       let numberOfDaysInThisMonth = Calendar.current.range(of: .day, in: .month, for: calcDate)?.count {
+                        for day in 1...numberOfDaysInThisMonth {
+                            var dateItemForThisDay = _DateItem(day: day, month: month, year: year)
+                            
+                            if let date = dateItemForThisDay.date {
+                                dateItemForThisDay.isEnabled = today <= endDate && (today...endDate).contains(date)
+                                dateItemForThisDay.isSelected = date <= today
+                            }
+                            seedData.append(dateItemForThisDay)
+                        }
+                    }
+                }
+                
+                startMonth = 1
+            }
+            
+            calendarView?.setupData = seedData
+            calendarView?.disabledAlpha = (.dark == traitCollection.userInterfaceStyle) ? 0.5 : 0.3   // More transparent, when light.
+        }
     }
 }
+
+/* ###################################################################################################################################### */
+// MARK: RVS_CalendarInputDelegate Conformance
+/* ###################################################################################################################################### */
+extension SwiftBMLSDK_TestHarness_VirtualCustom1ViewController: RVS_CalendarInputDelegate {
+    /* ################################################################## */
+    /**
+     This is called when the user selects an enabled calendar date.
+     Currently, only a console message is sent.
+     - parameter inCalendarInput: The calendar input widget instance. It is ignored in this handler.
+     - parameter dateItemChanged: The date item that was changed.
+     - parameter dateButton: The date item button that was actuated.
+     */
+    func calendarInput(_ inCalendarInput: RVS_CalendarInput, dateItemChanged inDateItem: RVS_CalendarInputDateItemProtocol, dateButton inDateButton: RVS_CalendarInput.DayButton?) {
+        print("The date \(String(describing: inDateItem.date)) was selected by the user. It is currently \(inDateItem.isSelected ? "" : "not ")selected.")
+    }
+}
+
